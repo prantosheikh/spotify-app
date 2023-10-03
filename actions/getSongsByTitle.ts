0b4d51a506af6d8704e3912c112f62f93ac2ee74
Pranto Sheikh <prantosheikh834@gmail.com>
2023-10-04 02:40:25 +0600

    user specific songs or perform actions like reacting, saving, or playing music

---
+++ actions/getSongsByTitle.ts
@@ -0,0 +1,33 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import getSongs from "./getSongs";
+
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  const { data: sessionData, error: sessionError } =
+    await supabase.auth.getSession();
+
+  if (!title) {
+    const allSong = await getSongs();
+    return allSong;
+  }
+
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .ilike("title", `%${title}%`)
+    .eq("user_id", sessionData.session?.user.id)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  return (data as any) || [];
+};
+
+export default getSongsByTitle;
---
+++ components/SearchInput.tsx
@@ -0,0 +1,36 @@
+"use client";
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import qs from "query-string";
+import { useEffect, useState } from "react";
+import Input from "./Input";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <div>
+      <Input
+        placeholder="What do you want to listen to ?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
